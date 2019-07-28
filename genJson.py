c6a4723d8ad58499544dd8db8c16e6e8c7bc0896
calebJohn <caleb@cedarrobots.com>
2019-07-27 17:18:35 -0700

    fixed inputting items

--- genJson.py
+++ genJson.py
@@ -1,64 +1,28 @@
 import json
-import math
-import itertools
+file = open("menu.json","r")
+data = json.load(file)
+print(len(data['items']))
+sizeArr = []
+extras = []
+name = input("enter item name ->")
+name = name.upper()
+numSizes = int(input("enter number of sizes ->"))
+for sz in range((numSizes)):
+    sizeName = input("enter size name (if only one size enter u) ->")
+    sizeName = sizeName.upper()
+    sizePrice = float(input("enter size price ->"))
+    sizeArr.append([sizeName,sizePrice])
+numExtras = int(input("enter number of extras ->"))
+for ex in range(numExtras):
+    extraName = input("enter extra/topping name (if none enter u) ->")
+    extraName = extraName.upper()
+    extraPrice = float(input("enter price ->"))
+    extras.append([extraName,extraPrice])
 
-def nCr(n,r):
-    f = math.factorial
-    return f(n) / f(r) / f(n-r)
-
-data = {}
-data['items'] = []
-while True:
-    sizes = []
-    extras = []
-    name = input("enter name enter DONE if finished>")
-    name = name.upper()
-    if(name == "DONE"):
-        print(data)
-        with open('menu.json', 'w') as outfile:
-            json.dump(data, outfile)
-        break
-    noSizes = int(input("enter number of sizes >"))
-    writeData = []
-    for sz in range(noSizes):
-       nameSize = input("enter size name if only one size enter N >")
-       nameSize = nameSize.upper()
-       if(nameSize == "N"):
-           nameSize = ""
-       priceSize = float(input("enter size price>"))
-       sizes.append([nameSize,priceSize])
-
-    noExtras = int(input("enter number of customizations if none enter N  >"))
-    for ex in range(noExtras):
-        nameExtra = input("enter customization name, enter 2 word pairaing with a hyphen ex. 'hot-sauce' >")
-        nameExtra = nameExtra.upper()
-        if (nameExtra == "N"):
-            nameExtra = ""
-        priceExtra = float(input("enter customization price >"))
-        extras.append([nameExtra,priceExtra])
-
-    itx = 0
-    for szx in range(len(sizes)):
-        while itx <= (len(extras)):
-            combos = list(itertools.combinations(extras, itx))
-            arr = []
-            for inx in range(len(combos)):
-                arr.append(list(combos[inx]))
-            for zx in range(len(arr)):
-                writestr = sizes[szx][0] + " " + name + " "
-                writeNum = sizes[szx][1]
-                for zxx in range(itx):
-                    writestr += arr[zx][zxx][0]
-                    writestr += " "
-                    writeNum += arr[zx][zxx][1]
-                    writestr.rstrip()
-                    writestr.lstrip()
-                data['items'].append({
-                    'name': str(writestr),
-                    'price':float(writeNum)
-                })
-            itx += 1
-        itx = 0
-
-
-
+data['items'].append({
+    'name': name,
+    'sizes': sizeArr,
+    'extras': extras
+})
+file = open("menu.json","w")
+json.dump(data, file)
